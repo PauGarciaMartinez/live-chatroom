8fdb928176eac20e4e39806b308f4a28ee66969f
Pau Garcia Martínez <etpau@hotmail.es>
2021-06-08 00:25:50 +0200

    Add some styles to the form components

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -2,6 +2,7 @@
   <div class="welcome container">
     <p>Welcome</p>
     <SignUpForm />
+    <LogInForm />
   </div>
 </template>
 
@@ -22,5 +23,22 @@ export default {
 .welcome {
   text-align: center;
   padding: 20px 0;
+}
+.welcome form {
+  width: 300px;
+  margin: 20px auto;
+}
+.welcome label {
+  display: block;
+  margin: 20px 0 10px;
+}
+.welcome input {
+  width: 100%;
+  padding: 10px;
+  border-radius: 20px;
+  border: 1px solid #eee;
+  outline: none;
+  color: #999;
+  margin: 10px auto;
 }
 </style>
