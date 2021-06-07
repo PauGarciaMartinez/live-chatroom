4c5e0167e98ba171d0f4d6a9487f76ef7c10b23e
Pau Garcia Martínez <etpau@hotmail.es>
2021-06-08 00:18:51 +0200

    Create sign up component and set a form

---
+++ src/components/SignUpForm.vue
@@ -0,0 +1,40 @@
+<template>
+
+  <form @submit.prevent="handleSubmit">
+    <input 
+      type="text" 
+      required 
+      placeholder="Display name" 
+      v-model="displayName">
+    <input 
+      type="email" 
+      required 
+      placeholder="Email" 
+      v-model="email">
+    <input 
+      type="password" 
+      required 
+      placeholder="Password" 
+      v-model="password">
+    <button>Sign Up</button>
+  </form>
+
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const displayName = ref('')
+    const email = ref('')
+    const password = ref('')
+
+    export { displayName, email, password }
+  }
+}
+</script>
+
+<style>
+
+</style>
