02b94d053650c0a0169151a954e7a5e580820124
Pau Garcia Martínez <etpau@hotmail.es>
2021-06-08 00:04:47 +0200

    Set global css file and add some style to welcome component

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Welcome from '../views/Welcome.vue'
 
 const routes = [
-
+  {
+    path: '/',
+    name: 'Welcome',
+    component: Welcome
+  }
 ]
 
 const router = createRouter({
