aa25f1062cb525b801962fd781d9f6eca2c4000d
Pau Garcia Martínez <etpau@hotmail.es>
2021-06-10 00:32:52 +0200

    Call sign in firebase method in a try catch block

--- src/composables/useLogIn.js
+++ src/composables/useLogIn.js
@@ -6,7 +6,14 @@ const error = ref(null)
 const logIn = async (email, password) => {
   error.value = null
 
-  
+  try {
+    const res = await projectAuth.signInWithEmailAndPassword(email, password)
+    error.value = null
+    
+  } catch(err) {
+    console.log(err.value)
+    error.value = 'Incorrect log in credentials'
+  }
 }
 
 const useLogIn = () => {
