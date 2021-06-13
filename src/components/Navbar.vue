8f951834e0c0cb6f8067df01945edf2924516770
Pau Garcia Martínez <etpau@hotmail.es>
2021-06-13 11:27:33 +0200

    Set signout method>

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,13 +4,26 @@
       <p>Hey there... display name here</p>
       <p class="email">Currently logged in as... email</p>
     </div>
-    <button>Log Out</button>
+    <button @click="handleClick">Log Out</button>
   </nav>
 </template>
 
 <script>
-export default {
+import useLogOut from '@/composables/useLogOut'
 
+export default {
+  setup() {
+    const { logOut, error } = useLogOut()
+
+    const handleClick = async () => {
+      await logOut()
+      if (!error.value) {
+        console.log('User logged out')
+      }
+    }
+
+    return { logOut, error, handleClick }
+  }
 }
 </script>
 
--- src/composables/useLogOut.js
+++ src/composables/useLogOut.js
@@ -4,7 +4,14 @@ import { projectAuth } from "../firebase/config"
 const error = ref(null)
 
 const logOut = async () => {
-
+  error.value = null
+  try {
+    await projectAuth.signOut()
+    
+  } catch(err) {
+    console.log(err.message)
+    error.value = err.message
+  }
 }
 
 const useLogOut = () => {
