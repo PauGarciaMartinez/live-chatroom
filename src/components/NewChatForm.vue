a486c756d1edd456410a6fbe83941f9c26565270
Pau Garcia Martínez <etpau@hotmail.es>
2021-06-14 16:39:51 +0200

    Create chat object filled when form message sent

--- src/components/NewChatForm.vue
+++ src/components/NewChatForm.vue
@@ -2,14 +2,34 @@
   <form>
     <textarea
       placeholder="Type a message and hit enter to send..."
-      v-model="message">
+      v-model="message"
+      @keypress.enter.prevent="handleSubmit">
     </textarea>
   </form>
 </template>
 
 <script>
-export default {
+import { ref } from '@vue/reactivity'
+import getUser from '../composables/getUser'
+import { timestamp } from '../firebase/config'
 
+export default {
+  setup() {
+    const { user } = getUser()
+    const message = ref('')
+
+    const handleSubmit = async () => {
+      const chat = {
+        name: user.value.displayName,
+        message: message.value,
+        createdAt: timestamp()
+      }
+    }
+
+    message.value = ''
+    
+    return { handleSubmit, message }
+  }
 }
 </script>
 
