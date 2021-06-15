e3da7374b18823812a8111ad3dc13cf21af722de
Pau Garcia Martínez <etpau@hotmail.es>
2021-06-15 23:52:45 +0200

    Create a chat window component to display real time messages

---
+++ src/components/ChatWindow.vue
@@ -0,0 +1,19 @@
+<template>
+  
+</template>
+
+<script>
+import getCollection from '@/composables/getCollection'
+
+export default {
+  setup() {
+    const { documents, error } = getCollection('messages')
+
+    return { documents, error }
+  }
+}
+</script>
+
+<style>
+
+</style>
