1e97fb3e9ed6e31a154432c69475d73e8205cc2c
Pau Garcia Martínez <etpau@hotmail.es>
2021-06-15 23:58:03 +0200

    Template component and cycle through documents array

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -1,5 +1,14 @@
 <template>
-  
+    <div class="chat-window">
+      <div v-if="error">{{ error }}</div>
+      <div v-if="documents" class="messages">
+        <div v-for="doc in documents" :key="document.id" class="single">
+          <span class="created-at">{{ doc.createdAt }}</span>
+          <span class="name">{{ doc.name }}</span>
+          <span class="message">{{ doc.message }}</span>
+        </div>
+      </div>
+    </div>
 </template>
 
 <script>
