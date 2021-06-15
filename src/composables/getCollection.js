1f0ef8163d05d1d9e298f7444e7d6092e87108ec
Pau Garcia Martínez <etpau@hotmail.es>
2021-06-15 23:50:42 +0200

    Config snapshot to store all results in object array

--- src/composables/getCollection.js
+++ src/composables/getCollection.js
@@ -13,7 +13,15 @@ const getCollection = (collection) => {
       snap.docs.forEach(doc => {
         doc.data().createdAt && results.push({ ...doc.data(), id: doc.id })
       })
+      documents.value = results
+      error.value = null
+    }, (err) => {
+      console.log(err.message) 
+      documents.value = null
+      error.value = 'Could not fetch data'
     })
+
+    return { documents, error }
 }
 
 export default getCollection
